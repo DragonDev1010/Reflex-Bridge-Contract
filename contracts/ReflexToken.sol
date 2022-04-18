c82a4d300621fade91e57546ab30e06ed387ba46
dragonyu1010 <provic0099@gmail.com>
2022-04-18 23:53:21 +0800

    change test token

--- contracts/ReflexToken.sol
+++ contracts/ReflexToken.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract ReflexToken is ERC20 {
-    constructor() ERC20("Zam Token", "ZAM") {
+    constructor() ERC20("Reflex Token", "REFLEX") {
         _mint(msg.sender, 10**23);
     }
 }
